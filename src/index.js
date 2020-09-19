d6b1491ae854141fe7881cf7ded6ef29ccf302ee
Aiden <dwx_job@163.com>
2020-09-19 10:09:20 +0800

    第1章clickDemo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './page/chapter1/start/App';
+import App from './page/chapter1/demo1/ClickCounterDemo';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
---
+++ src/page/chapter1/demo1/ClickCounterDemo.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import ClickCounter from '../../../component/ClickCounter'
+class ClickCounterDemo extends Component{
+  render(){
+    return (
+      <ClickCounter/>
+    )
+  }
+}
+export default ClickCounterDemo;
