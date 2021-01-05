378b505eacf91bc9b32bacf846163a724517f05e
aiden <dwx_job@163.com>
2021-01-05 22:57:02 +0800

    集成dva

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import dva from 'dva';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import createHistory from 'history/createBrowserHistory';
+import config from './utils/config';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export { config };
+// 1. Initialize
+const app = dva({
+  history: createHistory(),
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// 2. Plugins
+// app.use({});
+
+// 3. Model
+app.model(require('./models/app').default);
+
+// 4. Router
+app.router(require('./router').default);
+
+// 5. Start
+app.start('#root');
