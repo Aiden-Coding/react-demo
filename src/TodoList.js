5e61ad39073ab2851f079c30023b703c5903bb2e
aiden <dwx_job@163.com>
2021-01-13 21:27:19 +0800

    todo

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,32 +4,58 @@ class TodoList extends Component {
     super(props);
     this.state = {
       inputValue: '',
-      list: []
-    }
+      list: ['学习'],
+    };
   }
-  // handleInputChange(e) {
-  //   console.log(e.target.value)
-  // }
-  handleInputChange = (e) => {
+  handleInputChange(e) {
     this.setState({
-      inputValue: e.target.value
-    })
-    console.log(e.target.value)
+      inputValue: e.target.value,
+    });
+    console.log(e.target.value);
   }
+  handleBtnClick() {
+    this.setState({
+      list: [...this.state.list, this.state.inputValue],
+      inputValue: '',
+    });
+  }
+
+  // handleDeleteListItem(index){
+  //   console.log(index)
+  //  };
+  handleDeleteListItem = (index) => {
+    const tmpList = [...this.state.list]
+    tmpList.splice(index,1)
+    this.setState({
+      list: tmpList
+    })
+    console.log(index);
+  };
   render() {
     return (
       // 占位符
       <Fragment>
         <div>
-          <input value = {this.state.inputValue}
-            // onChange={this.handleInputChange.bind(this)}
-            onChange={this.handleInputChange}
+          <input
+            value={this.state.inputValue}
+            onChange={this.handleInputChange.bind(this)}
+            // onChange={()=>this.handleInputChange}
           />
-          <button>提交</button>
+          <button onClick={() => this.handleBtnClick()}>提交</button>
+          {/* <button onClick={this.handleBtnClick.bind(this)}>提交</button> */}
         </div>
         <ul>
-          <li>学习</li>
-          <li>工作</li>
+          {this.state.list.map((item, index) => {
+            return (
+              // <li key={index} onClick={this.handleDeleteListItem.bind(this,index)}>
+              //   {item}
+              // </li>
+              // <li key={index} onClick={()=>this.handleDeleteListItem(index)}>
+              <li key={index} onClick={() => this.handleDeleteListItem(index)}>
+                {item}
+              </li>
+            );
+          })}
         </ul>
       </Fragment>
     );
