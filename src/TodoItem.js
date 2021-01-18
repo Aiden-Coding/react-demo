c1459723ac6f55818de14e63dcc5b5a9760f65f0
aiden <dwx_job@163.com>
2021-01-18 22:49:37 +0800

    1

---
+++ src/TodoItem.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+class TodoItem extends Component {
+
+  render() {
+    return <div onClick={this.handleClick.bind(this)}>{this.props.content}</div>
+  }
+  handleClick() {
+    this.props.itemDelete(this.props.index)
+    // alert(this.props.index)
+  }
+}
+export default TodoItem;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,6 @@
 import React, { Component, Fragment } from 'react';
+import './style.css';
+import TodoItem from './TodoItem';
 class TodoList extends Component {
   constructor(props) {
     super(props);
@@ -24,11 +26,11 @@ class TodoList extends Component {
   //   console.log(index)
   //  };
   handleDeleteListItem = (index) => {
-    const tmpList = [...this.state.list]
-    tmpList.splice(index,1)
+    const tmpList = [...this.state.list];
+    tmpList.splice(index, 1);
     this.setState({
-      list: tmpList
-    })
+      list: tmpList,
+    });
     console.log(index);
   };
   render() {
@@ -36,7 +38,11 @@ class TodoList extends Component {
       // 占位符
       <Fragment>
         <div>
+          {/* htmlfor  聚焦 */}
+          <label htmlFor="insertArea">请输入内容</label>
           <input
+            id="insertArea"
+            className="input"
             value={this.state.inputValue}
             onChange={this.handleInputChange.bind(this)}
             // onChange={()=>this.handleInputChange}
@@ -51,9 +57,19 @@ class TodoList extends Component {
               //   {item}
               // </li>
               // <li key={index} onClick={()=>this.handleDeleteListItem(index)}>
-              <li key={index} onClick={() => this.handleDeleteListItem(index)}>
-                {item}
-              </li>
+              // <li
+              //   key={index}
+              //   onClick={() => this.handleDeleteListItem(index)}
+              //   // 渲染html脚本
+              //   dangerouslySetInnerHTML={{ __html: item }}
+              // >
+              //   {/* {item} */}
+              // </li>
+              <TodoItem
+                content={item}
+                index={index}
+                itemDelete={this.handleItemDelete.bind(this)}
+              />
             );
           })}
         </ul>
