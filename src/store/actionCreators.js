270ff81bf16150415cef951abeed1864cbd97c93
aiden <dwx_job@163.com>
2021-01-22 00:28:58 +0800

    redux-thunk

--- src/store/actionCreators.js
+++ src/store/actionCreators.js
@@ -1,5 +1,6 @@
 import { CHANGE_INPUT_INPUTVALUE, ADD_TODO_ITEM, DEL_DATA, CLEAN_DATA } from './actionType';
-
+import axios from 'axios';
+import store from './index';
 export const getInputChangeAction = (value) => ({
   type: CHANGE_INPUT_INPUTVALUE,
   value,
@@ -15,5 +16,25 @@ export const getDelData = (index) => ({
 });
 
 export const getCleanData = () => ({
-  type: CLEAN_DATA
+  type: CLEAN_DATA,
 });
+
+export const getAjax = (index) => ({
+  type: CLEAN_DATA,
+});
+export const getTodoList = () => {
+  // return () => {
+  //   axios.get('../../todoList.json').then((res) => {
+  //     const data = res.data;
+  //     const action = getAjax(data);
+  //     store.dispatch(action);
+  //   });
+  // };
+  return (dispatch) => {
+    axios.get('../../todoList.json').then((res) => {
+      const data = res.data;
+      const action = getAjax(data);
+      dispatch(action);
+    });
+  };
+};
