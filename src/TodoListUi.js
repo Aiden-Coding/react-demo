76096e097df1ccb4f40811e700cfe3cfb4f38c1d
aiden <dwx_job@163.com>
2021-01-21 23:59:54 +0800

    无状态组件

--- src/TodoListUi.js
+++ src/TodoListUi.js
@@ -2,28 +2,28 @@ import React, { Component } from 'react';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 import { DatePicker } from 'antd';
 import { Input, Button, List } from 'antd';
-class TodoListUi extends Component {
-  render() {
+const TodoListUi = (props) => {
+
     return (
       <React.Fragment>
         <h3 style={{ marginBottom: 16 }}>TodoList</h3>
         <Input
-          value={this.props.inputValue}
+          value={props.inputValue}
           placeholder="todolist输入框"
           style={{ width: '300px', marginRight: '10px' }}
-          onChange={this.props.handleChange}
+          onChange={props.handleChange}
         />
-        <Button type="primary" onClick={this.props.handleAdd}>
+        <Button type="primary" onClick={props.handleAdd}>
           提交
         </Button>
         <List
           style={{ width: '300px' }}
           bordered
-          dataSource={this.props.dataObj}
+          dataSource={props.dataObj}
           renderItem={(item, index) => (
             <List.Item
               onClick={(index) => {
-                this.props.handleDel(index);
+                props.handleDel(index);
               }}
             >
               {item}
@@ -32,6 +32,38 @@ class TodoListUi extends Component {
         />
       </React.Fragment>
     );
-  }
+
 }
+// class TodoListUi extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <h3 style={{ marginBottom: 16 }}>TodoList</h3>
+//         <Input
+//           value={props.inputValue}
+//           placeholder="todolist输入框"
+//           style={{ width: '300px', marginRight: '10px' }}
+//           onChange={props.handleChange}
+//         />
+//         <Button type="primary" onClick={props.handleAdd}>
+//           提交
+//         </Button>
+//         <List
+//           style={{ width: '300px' }}
+//           bordered
+//           dataSource={props.dataObj}
+//           renderItem={(item, index) => (
+//             <List.Item
+//               onClick={(index) => {
+//                 props.handleDel(index);
+//               }}
+//             >
+//               {item}
+//             </List.Item>
+//           )}
+//         />
+//       </React.Fragment>
+//     );
+//   }
+// }
 export default TodoListUi;
