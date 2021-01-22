c45b4803c9580e3469598712b4580af0d52c1558
Aiden <dwx_job@163.com>
2021-01-23 02:07:40 +0800

    redux

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { Globalstyle } from './style';
 import { IconGlobalstyle } from './static/iconfont/iconfont';
 import Header from './common/header';
+import store from './store';
+import { Provider } from 'react-redux';
 function App() {
   return (
-    <div>
-      <Globalstyle />
-      <IconGlobalstyle />
-      <Header/>
-    </div>
+    <Provider store={store}>
+      <div>
+        <Globalstyle />
+        <IconGlobalstyle />
+        <Header />
+      </div>
+    </Provider>
   );
 }
 
